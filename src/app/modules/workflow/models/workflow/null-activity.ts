b7ad4d76df8696d4de1be5c85cd2e149250de2f3
Ettiene Mare <ettienemare@gmail.com>
2018-03-01 21:16:33 +0200

    Add nova core and analytics

--- src/app/modules/workflow/models/workflow/null-activity.ts
+++ src/app/modules/workflow/models/workflow/null-activity.ts
@@ -2,13 +2,15 @@ import {Activity} from "./activity";
 import {WorkflowEvents} from "./workflow-events";
 import {Workflow} from "./workflow";
 
-export class EmptyActivity extends Activity {
-  constructor(private workFlow: Workflow) {
+export class NullActivity extends Activity {
+  constructor(private workFlow: Workflow, name: string) {
     super();
+
+    this.name = name;
   }
 
   execute(parameters: any) {
-    WorkflowEvents.activityNotFound(this.workFlow.workflowId, this.workFlow.name, name);
+    WorkflowEvents.activityNotFound(this.workFlow.workflowId, this.workFlow.name, this.name);
     this.notificationService.error("Workflow", `Activity with name ${this.name} was not found`);
   }
 }
--- src/app/modules/workflow/models/workflow/workflow.ts
+++ src/app/modules/workflow/models/workflow/workflow.ts
@@ -1,5 +1,5 @@
 import {WorkflowEvents} from "./workflow-events";
-import {EmptyActivity} from "./empty-activity";
+import {NullActivity} from "./null-activity";
 
 export class Workflow {
   activities = [];
@@ -9,7 +9,7 @@ export class Workflow {
 
   next(name: string) {
       const filter = this.activities.filter((act: any) => act.name == name);
-      const activity = filter.length > 0 ? filter[0] : new EmptyActivity(this);
+      const activity = filter.length > 0 ? filter[0] : new NullActivity(this, name);
 
       WorkflowEvents.changingState(this.workflowId, this.name, name, activity.constructor.name);
       filter[0].execute();
